220566eb3de0efaf55afa4e9c67713d60f6054a1
Arshd <artourky@yahoo.com>
2016-01-30 23:22:15 +0200

    dude do the git add thing

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        print("Entered");
+        collider.gameObject.SetActive(false);
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        print("Exitted");
+    }
+}
