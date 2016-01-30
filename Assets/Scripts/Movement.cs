9b92330523c04386756f62061b52d5d05f93fa36
Arshd <artourky@yahoo.com>
2016-01-30 08:04:42 +0200

    Baka no emacs 3andy :'(

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float xAxis, yAxis, moveSpeed = 5f;
+
+    private Rigidbody _rigidB;
+
+	// Use this for initialization
+	void Start () {
+        _rigidB = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        xAxis = Input.GetAxis("Horizontal");
+        yAxis = Input.GetAxis("Vertical");
+
+        _rigidB.velocity = new Vector3(xAxis * moveSpeed, 0f, yAxis * moveSpeed);
+	}
+}
